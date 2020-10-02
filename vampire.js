8e968d9aef1d70933b6cebc1e6f28d377130bc44
amarkham1 <andrewjmarkham1@gmail.com>
2020-10-02 00:20:43 +0000

    passed all tests

--- vampire.js
+++ vampire.js
@@ -10,22 +10,38 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfParents = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfParents++;
+    }
+
+    return number;
   }
-
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let currentVampire = this;
+    while (currentVampire.creator && vampire.creator) {
+      currentVampire = currentVampire.creator;
+      vampire = vampire.creator;
+    }
+    if (currentVampire.creator) {
+      return false;
+    }
+    return true;
   }
 
   /** Stretch **/
@@ -36,7 +52,29 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this.name === "root" || this === vampire) {
+      return this;
+    }
+    let currentVampire = this;
+    let commonVampires = {};
+    while (currentVampire.creator) {
+      commonVampires[currentVampire.name] = currentVampire.name;
+      currentVampire = currentVampire.creator;
+    }
+    commonVampires[currentVampire.name] = currentVampire.name;
 
+    while (vampire.creator) {
+      if (commonVampires[vampire.name]) {
+        return vampire;
+      }
+      vampire = vampire.creator;
+    }
+    if (commonVampires[vampire.name]) {
+      return vampire;
+    }
+
+
+    return null;
   }
 }
 
